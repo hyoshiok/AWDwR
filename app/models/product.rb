aed15789e72502cf6ea2187880900ec0141e27b7
Hiro Yoshioka <hyoshiok@gmail.com>
2013-05-04 13:49:58 +0900

    Validation!

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,10 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  validates :title, :description, :image_url, :presence => true
+  validates :title, :uniqueness => true
+  validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
+  validates :image_url, :format => {
+    :with => %r{\.(gif|jpg|png)$}i,
+    :message => 'must be a URL for GIF, JPG or PNG image'
+  }
 end
