b86db36cf7abd1b6aaca92aa139ab45f610bd596
Hiro Yoshioka <hyoshiok@gmail.com>
2013-05-05 12:06:46 +0900

    Catalog Display

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,5 +1,6 @@
 class Product < ActiveRecord::Base
   attr_accessible :description, :image_url, :price, :title
+  default_scope :order => 'title'
   validates :title, :description, :image_url, :presence => true
   validates :title, :uniqueness => true
   validates :price, :numericality => {:greater_than_or_equal_to => 0.01}
