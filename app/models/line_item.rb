2bc417403b3f95f7ac2b4028bcfe811042aeaa96
Hiro Yoshioka <hyoshiok@gmail.com>
2013-05-05 17:40:28 +0900

    Smater Cart

--- app/models/line_item.rb
+++ app/models/line_item.rb
@@ -2,4 +2,8 @@ class LineItem < ActiveRecord::Base
   attr_accessible :cart_id, :product_id
   belongs_to :product
   belongs_to :cart
+  
+  def total_price
+    product.price * quantity
+  end
 end
